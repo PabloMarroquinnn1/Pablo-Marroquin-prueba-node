ecebdeca717f7c16f697ed2d7bcb50f287373c67
PABLO ALEJANDRO MARROQUIN CUTZ <3617319650101@ingenieria.usac.edu.gt>
2026-01-27 20:25:41 -0600

    Endpoin categorias
    Implementacion del enpoint para poder ver las categorias de los productos

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const productosRoutes = require('./productos');
 const ventasRoutes = require('./ventas');
+const categoriasRoutes = require('./categorias');
 
 router.use('/productos', productosRoutes);
 router.use('/ventas', ventasRoutes);
+router.use('/categorias', categoriasRoutes);
 
 router.get('/', (req, res) => {
   res.json({
@@ -12,7 +14,8 @@ router.get('/', (req, res) => {
     version: '1.0.0',
     endpoints: {
       productos_con_stock: 'GET /api/productos',
-      top_10_mas_vendidos: 'GET /api/ventas/productos-mas-vendidos'
+      top_10_mas_vendidos: 'GET /api/ventas/productos-mas-vendidos',
+      categorias_con_productos: 'GET /api/categorias'
     }
   });
 });
